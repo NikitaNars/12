1b15aec138583b6081c9ce60985b5cec5987375b
Nikita Narseev <nne150307@gmail.com>
2025-05-21 01:06:20 +0500

    asd

--- 12/MainWindow.xaml.cs
+++ 12/MainWindow.xaml.cs
@@ -20,9 +20,82 @@ namespace _12
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<ToDo> TodoItems;
         public MainWindow()
         {
+
             InitializeComponent();
+            TodoItems = new List<ToDo>
+            {
+                new ToDo("Зdas", new DateTime(2024, 1, 15), "asd"),
+                new ToDo("asd", new DateTime(2024, 1, 12), "Мdas"),
+                new ToDo("sda", new DateTime(2024, 1, 20), "asd")
+            };
+            descriptionToDo.Text = "Описания нет";
+            dateToDo.SelectedDate = new DateTime(2024, 1, 10);
+
+
+            listToDo.ItemsSource = TodoItems;
+
+
+            listToDo.ItemsSource = null;
+            listToDo.ItemsSource = TodoItems;
+
+        }
+        private void AddItem(object sender, RoutedEventArgs e)
+        {
+            string title = titleToDo.Text;
+            DateTime date = (DateTime)dateToDo.SelectedDate;
+            string description = descriptionToDo.Text;
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                MessageBox.Show("Введите название дела");
+                return;
+            }
+
+
+            TodoItems.Add(new ToDo(title, date, description));
+
+            listToDo.Items.Refresh();
+
+            titleToDo.Text = null;
+            dateToDo.SelectedDate = DateTime.Now;
+            descriptionToDo.Text = "Описания нет";
+
+            listToDo.ItemsSource = null;
+            listToDo.ItemsSource = TodoItems;
+        }
+        private void KillItem(object sender, RoutedEventArgs e)
+        {
+            ToDo selectedToDo = listToDo.SelectedItem as ToDo;
+            if (selectedToDo == null)
+            {
+                MessageBox.Show("Выберите дело для удаления");
+                return;
+            }
+
+            TodoItems.Remove(selectedToDo);
+
+            listToDo.ItemsSource = null;
+            listToDo.ItemsSource = TodoItems;
+        }
+        private void CheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            if (groupBoxToDo == null) return;
+            CheckBox checkBox = sender as CheckBox;
+            if (checkBox != null)
+            {
+                if (checkBox.IsChecked == true)
+                {
+                    groupBoxToDo.Visibility = Visibility.Visible; 
+                }
+                else
+                {
+                    groupBoxToDo.Visibility = Visibility.Collapsed;
+                }
+
+            }
+
         }
     }
 }
